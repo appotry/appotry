74fe4d6d6e7316567c5b9910229de3f91392217c
夜法之书(appotry) <andycrusoe@gmail.com>
2023-03-30 00:10:30 +0800

    fix

--- generator.py
+++ generator.py
@@ -13,6 +13,7 @@ import datetime
 import requests
 from bs4 import BeautifulSoup
 import markdown
+from tabulate import tabulate
 
 headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36'
