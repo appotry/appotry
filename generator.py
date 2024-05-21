6389ea9760a7588f0cd16dbc9074f870a034fcd3
夜法之书(appotry) <andycrusoe@gmail.com>
2024-05-22 01:23:34 +0800

    fix

--- generator.py
+++ generator.py
@@ -97,10 +97,10 @@ def loadPostsByRSS():
         return []
 
 # 常量定义
-# POSTS_RSS_URL = 'https://17lai.netlify.app/rss.xml'
+POSTS_RSS_URL = 'https://17lai.netlify.app/rss.xml'
 
 # 使用 cloudflare 时，需要使用cloudscraper 来代替 requests
-POSTS_RSS_URL = 'https://blog.17lai.site/rss.xml'
+# POSTS_RSS_URL = 'https://blog.17lai.site/rss.xml'
 
 TO_REPLACE_POSTS = '{{Recent Posts}}'
 TO_REPLACE_DATE = '{{Generated At}}'
